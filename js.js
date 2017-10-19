7e3058034d31df19643c5db97a3dd5e79e41e747
Karar Haider <karar3k@yahoo.com>
2017-10-20 00:59:30 +0300

    This Is My Project

---
+++ js.js
@@ -0,0 +1,26 @@
+
+			function addition(){
+				var number_one_js = document.getElementById("number_one").value;
+				var number_two_js = document.getElementById("number_two").value;
+				var ans_js = Number(number_one_js) + Number(number_two_js);
+				var x = document.getElementById("Answer").innerHTML = ans_js; 
+			}
+			function subtraction(){
+				var number_one_js = document.getElementById("number_one").value;
+				var number_two_js = document.getElementById("number_two").value;
+				var ans_js = Number(number_one_js) - Number(number_two_js);
+				var x = document.getElementById("Answer").innerHTML = ans_js; 
+			}
+			function multiplication(){
+				var number_one_js = document.getElementById("number_one").value;
+				var number_two_js = document.getElementById("number_two").value;
+				var ans_js = Number(number_one_js) * Number(number_two_js);
+				var x = document.getElementById("Answer").innerHTML = ans_js; 
+			}
+			function division(){
+				var number_one_js = document.getElementById("number_one").value;
+				var number_two_js = document.getElementById("number_two").value;
+				var ans_js = Number(number_one_js) / Number(number_two_js);
+				var x = document.getElementById("Answer").innerHTML = ans_js; 
+			}
+			
